b18122b2d6e09ef8f9182834daec070b5ee61a48
“Mia <mb.mia.bosse@gmail.com>
2023-02-20 10:28:13 +0100

    font

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -3,17 +3,19 @@ module.exports = {
   content: ["./src/**/*.{html,vue,js,ts,jsx,tsx}"],
   theme: {
     colors: {
-primary: "#f4f4f4", 
-bg: "131313", 
-yellow: "d8fc16",
-green: "8fe30f",
-pink: "fe0194", 
-purple: "552b43",
+      primary: "#f4f4f4",
+      bg: "131313",
+      yellow: "d8fc16",
+      green: "8fe30f",
+      pink: "fe0194",
+      purple: "552b43",
     },
 
     fontFamily: {
+      quicksand: "'Quicksand', sans-serif;",
+      roboto: "'Roboto', sans-serif;",
+    },
 
-    }
     extend: {},
   },
   plugins: [require("@tailwindcss/typography"), require("@tailwindcss/forms")],
