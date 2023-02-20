70eecf0e64e17acc9a8779bb38664e70c5468267
“Mia <mb.mia.bosse@gmail.com>
2023-02-20 11:22:27 +0100

    video

--- src/main.js
+++ src/main.js
@@ -3,6 +3,18 @@ import "./style.css";
 /* Import af Motion One bibliotek */
 import { animate, stagger, inView, scroll, timeline } from "motion";
 
-
-
 scroll(animate(".progress-bar", { scaleX: [0, 1] }));
+
+const lillevideo = document.querySelector("#videofil");
+lillevideo.pause();
+
+scroll(
+  ({ y }) => {
+    if (!lillevideo.readyState) return;
+    lillevideo.currentTime = lillevideo.duration * y.progress;
+  },
+  {
+    target: document.querySelector(".videoarticle"),
+    offset: ["-100vh", "end end"],
+  }
+);
