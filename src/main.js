be8da4c9b7a15826ac96b61d019862a7190faff9
“Mia <mb.mia.bosse@gmail.com>
2023-02-20 12:14:49 +0100

    glitch

--- src/main.js
+++ src/main.js
@@ -5,16 +5,20 @@ import { animate, stagger, inView, scroll, timeline } from "motion";
 
 scroll(animate(".progress-bar", { scaleX: [0, 1] }));
 
-const lillevideo = document.querySelector("#videofil");
-lillevideo.pause();
+const videocontainer = document.querySelectorAll(".videocontainer");
 
-scroll(
-  ({ y }) => {
-    if (!lillevideo.readyState) return;
-    lillevideo.currentTime = lillevideo.duration * y.progress;
-  },
-  {
-    target: document.querySelector(".videoarticle"),
-    offset: ["-100vh", "end end"],
-  }
-);
+videocontainer.forEach((video) => {
+  const lillevideo = video.querySelector(".lillevideo");
+  lillevideo.pause();
+
+  scroll(
+    ({ y }) => {
+      if (!lillevideo.readyState) return;
+      lillevideo.currentTime = lillevideo.duration * y.progress;
+    },
+    {
+      target: video.querySelector(".videoarticle"),
+      offset: ["-100vh", "end end"],
+    }
+  );
+});
